d8ddbbc42ec38c9ac2091fb771ac0431a368fd32
Bogdan Florea <bogd.florea@gmail.com>
2025-05-05 19:39:26 +0300

    cleaning up stuff and updating readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 The script parses through a log file and marks jobs which took longer than 5 minutes as WARNING and jobs which took longer than 10 minutes as ERRORS.
+
+parse_log function parses the csv file and removes whitespaces
+
+analyze_jobs function analyzes the entries and writes the report
--- logparser.py
+++ logparser.py
@@ -14,19 +14,19 @@ def parse_log(file_path):
         reader = csv.reader(csvfile)
         for row in reader:
             timestamp, task_name, status, pid = row
-            #cleaing up empty spaces
+            #cleaning up empty spaces
             pid = str(pid).strip()
             status = status.strip()
             timestamp = datetime.strptime(timestamp.strip(), "%H:%M:%S")
-
+            #checking if pid is already in jobs dict
             if pid not in jobs:
                 jobs[pid] = {"name": task_name, "start": None, "end": None}
-
+            #updating start and end times for each job
             if status == "START":
                 jobs[pid]["start"] = timestamp
             elif status == "END":
                 jobs[pid]["end"] = timestamp
-            print(f"DEBUG: {timestamp} - {task_name} - {status} - {pid}") # Debugging print
+            print(f"DEBUG: {timestamp} - {task_name} - {status} - {pid}") # Debugging print of elements of dict
 
     return jobs
 
@@ -34,16 +34,15 @@ def parse_log(file_path):
 
 def analyze_jobs(jobs, report):
     with open(report, "w") as f:
-#add identified jobs to report
+        #add identified jobs to report
         f.write("Jobs...\n")
-#going through each job by pid
+        #going through each job by pid and writing to output report the job name, start and end time
         for pid, details in jobs.items():
-            print(f"DEBUG2: {pid} - {details}") # Debugging print
             startt = details["start"].strftime("%H:%M:%S") if details["start"] else "none"
             endd = details["end"].strftime("%H:%M:%S") if details["end"] else "none"
             f.write(f"- Job {details['name']} (PID {pid}) | Start: {startt} | End: {endd}\n")
 
-#analyzing jobs and writing to report
+        #analyzing jobs and writing to report
         f.write("\nAnalysis:\n")
         for pid, details in jobs.items():
             if details["start"] and details["end"]:
@@ -55,13 +54,8 @@ def analyze_jobs(jobs, report):
                     f.write(f"ERROR: Job {details['name']} (PID {pid}) took {duration_str}!\n")
                 elif duration > WARNING_THRESHOLD:
                     f.write(f"WARNING: Job {details['name']} (PID {pid}) took {duration_str}!\n")
-                else:
-                    f.write(f"INFO: Job {details['name']} (PID {pid}) completed in {duration_str}.\n")
-
-            # f.write(f"- Job {details['name']} (PID {pid}) | Start: {start} | End: {end}\n")
-
-#debugging print
-            # print(f"- Job {details['name']} (PID {pid}) | Start: {start} | End: {end}\n")
+                """ else:
+                    f.write(f"INFO: Job {details['name']} (PID {pid}) completed in {duration_str}.\n") """
 
 
 if __name__ == "__main__":
